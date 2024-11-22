32ba95fcc3bb09187b8403cee4931ec183b99641
OnnRent <vkarnwal8279@gmail.com>
2024-11-22 12:41:23 +0530

    Update Signup.jsx

--- FrontEnd/src/Components/Signup/Signup.jsx
+++ FrontEnd/src/Components/Signup/Signup.jsx
@@ -29,7 +29,7 @@ const Signup = () => {
         const {name, email , password}=signUpInfo;
 
         try{
-            const url ="https://rent-api-pi.vercel.app/signup";
+            const url ="https://rent-six-phi.vercel.app/auth/signup";
             const response = await fetch(url,{
                 method:"POST",
                 headers:{
